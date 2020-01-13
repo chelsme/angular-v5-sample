3d881e17a1cba53d65be277ce821e5da7705e95f
Crowson, Chelsea <chelsea.crowson@accenture.com>
2020-01-13 13:14:16 -0600

    routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { BodyComponent } from "./body/body.component";
+import { EasterEggComponent } from "./easter-egg/easter-egg.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: BodyComponent
+  },
+  {
+    path: "easter-egg",
+    component: EasterEggComponent
+  }
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HighlightDirective } from "./highlight.directive";
-import { BodyComponent } from './body/body.component';
+import { BodyComponent } from "./body/body.component";
+import { AppRoutingModule } from './/app-routing.module';
+import { EasterEggComponent } from './easter-egg/easter-egg.component';
 
 @NgModule({
-  declarations: [AppComponent, HighlightDirective, BodyComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HighlightDirective, BodyComponent, EasterEggComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
