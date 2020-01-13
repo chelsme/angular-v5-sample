509ea75c5631ce4e5dcb283375972bc4c154f264
Crowson, Chelsea <chelsea.crowson@accenture.com>
2020-01-13 11:19:26 -0600

    restructure and add click event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HighlightDirective } from "./highlight.directive";
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
-  declarations: [AppComponent, HighlightDirective],
+  declarations: [AppComponent, HighlightDirective, BodyComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-body",
+  templateUrl: "./body.component.html",
+  styleUrls: ["./body.component.css"]
+})
+export class BodyComponent implements OnInit {
+  value = "one way binding";
+  twoWayBinding = "two way binding";
+  goalCount: number;
+  goals = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.goalCount = this.goals.length;
+  }
+
+  submitForm() {
+    this.goals.push(this.twoWayBinding);
+    this.twoWayBinding = "";
+    this.goalCount = this.goals.length;
+  }
+}
