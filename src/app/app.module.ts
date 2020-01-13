b1f4af3a1d3d0af179243fc2ff26419d34c38f86
Crowson, Chelsea <chelsea.crowson@accenture.com>
2020-01-13 11:02:01 -0600

    setup for 2 way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-import { HighlightDirective } from './highlight.directive';
-
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HighlightDirective } from "./highlight.directive";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HighlightDirective
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HighlightDirective],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
